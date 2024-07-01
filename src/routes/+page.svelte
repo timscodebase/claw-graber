a217edc20cd21b8b99d16e9dbe4b77c2c3202549
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-06-30 18:07:11 -0700

    chore: Remove unnecessary files and update dependencies

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button
+	class="border rounded p-2 mt-10 bg-gray-800 text-white hover:bg-gray-700"
+	on:click={() => (window.location.href = '/auth')}
+>
+	Logout
+</button>
--- src/routes/api/users/+server.ts
+++
@@ -1,7 +0,0 @@
-import pb from "$lib/pb";
-
-// /api/newsletter GET
-
-export async function GET() {
-  return await pb.collection("users").getFullList();
-}
